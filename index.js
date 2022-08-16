4ce0a2db0519028b39a6354f3bf56cd26d466047
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-15 23:24:13 -0400

    editing prompts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = ({ name, location, github, linkedin }) =>
+function generateHTML (data) {
   `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,57 +24,62 @@ const generateHTML = ({ name, location, github, linkedin }) =>
 </div>
 </body>
 </html>`;
+}
+function main () {
+    return inquirer
+        .prompt(
+            {
+                type: 'list',
+                name: 'mainMenu',
+                message: 'What would you like to do?',
+                choice: ['Add team member', 'Finish building team']
+            },
+        )
+        .then(response => {
+            switch (response.mainMenu) {
+    
+                case 'Add team member':
+                    addTeam();
+                    break;
+                case 'Finish building team':
+                    finishTeam();
+                    break;
+            }
+        });
+}    
+    
+function addTeam () {
+    
+}
 
-inquirer
-    .prompt(
-        {
-            type: 'list',
-            name: 'mainMenu',
-            message: 'What would you like to do?',
-            choice: ['Add team member', 'Finish building team']
-        },
-    )
-    .then(response => {
-        switch (response.mainMenu) {
+        // {
+        //   type: 'input',
+        //   name: 'name',
+        //   message: 'What is the name of your team member?',
+        // },
+        // {
+        //   type: 'list',
+        //   name: 'role',
+        //   message: 'What is the role of your team member?',
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'id',
+        //   message: 'What is the ID number of your team member?',
+        // },
+        // {
+        //   type: 'input',
+        //   name: 'email',
+        //   message: 'What is the email of your team member?',
+        // },
 
-            case 'Add team member':
-                addTeam();
-                break;
-            case 'Finish building team':
-                finishTeam();
-                break;
-        }
-    });
-    // {
-    //   type: 'confirm',
-    //   name: 'addEngineer',
-    //   message: 'Would you like to add an engineer?',
-    // },
-    // {
-    //   type: 'input',
-    //   name: 'engineerName',
-    //   message: 'What is the name of the engineer?',
-    // },
-    // {
-    //   type: 'confirm',
-    //   name: 'addIntern',
-    //   message: 'Would you like to add an intern?',
-    // },
-    // {
-    //   type: 'input',
-    //   name: 'internName',
-    //   message: 'What is the name of the intern?',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'anotherIntern',
-    //     message: 'What is the name of the intern?',
-    // },   
 
-.then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+// .then(data => {
+//     const htmlPageContent = generateHTML(data);
 
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
-    );
-});
+//     fs.writeFile('index.html', htmlPageContent, (err) =>
+//       err ? console.log(err) : console.log('Successfully created index.html!')
+//     );
+// });
+
+main ();
