149ddcd321248df0987149f208f43715a9ce2847
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-17 09:44:43 -0400

    correctly linked variables on generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,7 +2,7 @@ const Engineer = require("../lib/Engineer");
 
 // html template function
 function generateHTML (employees) {
-  console.log(employees)
+
     const starting = `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -16,38 +16,38 @@ function generateHTML (employees) {
     </body>
     </html>`
     let people = ""
-    // big shoutout to Voravich S.whose repo I looked at for understanding of this logic structure
+    // big shoutout to Voravich S. whose repo I looked at for understanding of this logic structure
     // switch case for adding card by role
       employees.forEach(employee => {
         switch (employee.getRole()) {
           case "Manager":
             people += `    <div class="card" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title">${employees.inputName}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">${employees.role}</h6>
-              <h6 class="card-subtitle mb-2 text-muted">${employees.id}</h6>
-              <a href="#" class="card-link">${employees.email}</a>
-              <a href="#" class="card-link">${employees.officeNum}</a>
+              <h5 class="card-title">${employee.inputName}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Manager</h6>
+              <h6 class="card-subtitle mb-2 text-muted">${employee.id}</h6>
+              <a href="mailto:${employee.email}" class="card-link">${employee.email}</a>
+              <a href="#" class="card-link">Office Number: ${employee.officeNum}</a>
             </div>`
             break;
           case "Engineer":
             people += `    <div class="card" style="width: 18rem;">
             <div class="card-body">
-              <h5 class="card-title">${employees.inputName}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">${employees.role}</h6>
-              <h6 class="card-subtitle mb-2 text-muted">${employees.id}</h6>
-              <a href="#" class="card-link">${employees.email}</a>
-              <a href="#" class="card-link">${employees.github}</a>
+              <h5 class="card-title">${employee.inputName}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Engineer</h6>
+              <h6 class="card-subtitle mb-2 text-muted">${employee.id}</h6>
+              <a href="mailto:${employee.email}" class="card-link">${employee.email}</a>
+              <a href="${employee.github}" class="card-link">GitHub</a>
             </div>`
             break;
           case "Intern":
           people += `    <div class="card" style="width: 18rem;">
           <div class="card-body">
-            <h5 class="card-title">${employees.inputName}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">${employees.role}</h6>
-            <h6 class="card-subtitle mb-2 text-muted">${employees.id}</h6>
-            <a href="#" class="card-link">${employees.email}</a>
-            <a href="#" class="card-link">${employees.school}</a>
+            <h5 class="card-title">${employee.inputName}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Intern</h6>
+            <h6 class="card-subtitle mb-2 text-muted">${employee.id}</h6>
+            <a href="mailto:${employee.email}" class="card-link">${employee.email}</a>
+            <a href="#" class="card-link">${employee.school}</a>
           </div>`
             break;
           default:
